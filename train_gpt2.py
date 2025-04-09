2e91a82ff6a81e108b9607b9681560fc8c4cba19
Zhiyang Wang <3254247074@qq.com>
2025-04-09 23:14:01 +0800

    [2401210164] 增加了一个DataLoaderLite类，从训练数据文本文件中取token

--- train_gpt2.py
+++ train_gpt2.py
@@ -161,6 +161,38 @@ class GPT(nn.Module):
                     sd[k].copy_(sd_hf[k])
 
         return model
+
+# ----------------------------------------------------------------------------
+import tiktoken
+
+class DataLoaderLite:
+    def __init__(self,B,T):
+        self.B=B
+        self.T=T
+
+        # 从硬盘加载数据，存到内存中
+        with open('input.txt','r') as f:
+            text=f.read()
+        enc=tiktoken.get_encoding('gpt2')
+        tokens=enc.encode(text)
+        self.tokens=torch.tensor(tokens)
+        print(f"loaded {len(self.tokens)} tokens")
+        print(f"1 epoch = {len(self.tokens) // (B * T)} batches")
+
+        # state
+        self.current_position=0
+
+    def next_batch(self):
+        B,T=self.B,self.T
+        buf=self.tokens[self.current_position: self.current_position+B*T+1]
+        x=(buf[:-1]).view(B,T) # 输入
+        y=(buf[1:]).view(B,T) # targets
+        # 前移current_position位置，为下一次取token准备
+        self.current_position+=B*T
+        # 如果下一次取样超出数据集，复位起始索引
+        if self.current_position+(B*T+1)>len(self.tokens):
+            self.current_position=0
+        return x,y
     
 # ----------------------------------------------------------------------------
 # 生成前自动检测设备
@@ -171,18 +203,7 @@ elif hasattr(torch.backends,"mps") and torch.backends.mps.is_available():
     device="mps"
 print(f"using device: {device}")
 
-# get a data batch
-import tiktoken
-enc=tiktoken.get_encoding('gpt2')
-with open('input.txt','r') as f:
-    text=f.read()
-text=text[:1000]
-tokens=enc.encode(text)
-B,T=4,32
-buf=torch.tensor(tokens[:B*T+1])
-buf=buf.to(device)
-x=buf[:-1].view(B,T)
-y=buf[1:].view(B,T)
+train_loader=DataLoaderLite(B=4,T=32)
 
 # 计算由x预测出的logits
 model=GPT(GPTConfig())
@@ -191,6 +212,8 @@ model.to(device)
 # 优化！梯度下降
 optimizer=torch.optim.AdamW(model.parameters(),lr=3e-4)
 for i in range(50):
+    x,y=train_loader.next_batch()
+    x,y=x.to(device),y.to(device)
     optimizer.zero_grad()
     logits,loss=model(x,y)
     loss.backward()
